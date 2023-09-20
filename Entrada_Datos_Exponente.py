8c8bc38be6826de57c0fa4e4bfc5aa50f1ea9441
librelim <librelim@gmail.com>
2023-09-20 14:33:36 -0600

    commit de nuevo ejercicio de calculo exponente

---
+++ Entrada_Datos_Exponente.py
@@ -0,0 +1,10 @@
+#Sistema para Calcular el Cuadrado de un Número
+
+nombre = input("¿Cúal es tu nombre? ")
+print ("Hola " + nombre + ", vamos a calcular el cuadrado de un número ")
+num_uno = int(input("Por favor introduce el primer valor: "))
+exponente = 2
+resultado = num_uno ** exponente
+
+print (nombre + " el resultado del calculo del cuadrado del número es: " , resultado)
+print ("Fin.")
---
+++ Mi Primer Proyecto.py
@@ -0,0 +1,6 @@
+#Con este comando puedes colocar el número deseado y al correr el programa mostrará la elevación al Cuadrado del mismo
+print ("exponente:")
+numero_uno = 10
+exponente = 2
+resultado = numero_uno ** exponente
+print (resultado)
